e0b1a3f8506b311266e84588b61eed034a8b1e06
Guilherme Lima <guilhermelimaesilva.p@gmail.com>
2023-07-12 20:34:34 -0300

    Alternado JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-novoMedico.onclick = function() {
+novoMedico.onclick = () => {
     overlay.classList.add('active')
     drawer.classList.add('active')
 }
+
+fecharCriar.onclick = () => {
+    setTimeout (() => {
+        overlay.classList.remove('active')}, 500)
+    drawer.classList.remove('active')
+}
+
+overlay.onclick = () => {
+    setTimeout (() => {
+        overlay.classList.remove('active')}, 500)
+    drawer.classList.remove('active')
+}
+
+// 5274 9613 1689 1929 0330 0308
